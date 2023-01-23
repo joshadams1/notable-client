7833ced5788c0dbbfc56c62f246b1bb332203d9b
josh.adams@mirpartners.com <josh.adams@mirpartners.com>
2023-01-23 11:34:45 -0800

    Add table to doctors

--- src/Doctors.jsx
+++ src/Doctors.jsx
@@ -1,8 +1,21 @@
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
 import { useState, useEffect } from 'react';
+import { Grid, GridItem } from '@chakra-ui/react';
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+} from '@chakra-ui/react'
 
 function Doctors() {
     const [doctors, setDoctors] = useState([]);
+    const [appointments, setAppointments] = useState([]);
 
     useEffect(() => {
         async function fetchData() {
@@ -14,19 +27,64 @@ function Doctors() {
         fetchData();
     }, []);
 
+    const grabAppointments = async (id) => {
+        const res = await fetch(`http://localhost:8000/doctors/${id}/appointments`);
+        const data = await res.json();
+        setAppointments(data);
+    }
+
     return (
         <Tabs isFitted variant='enclosed'>
-            <TabList>
-                {doctors.map((doctor, idx) => {
-                    return (<Tab style={{marginRight: "10px"}} key={idx}>{doctor.firstName} {doctor.lastName}</Tab>)
-                })}
-            </TabList>
-
-            <TabPanels>
-                {doctors.map((doctor, idx) => {
-                    return (<TabPanel style={{marginTop: "10px"}} key={idx}>{doctor.email}</TabPanel>)
-                })}
-            </TabPanels>
+            <Grid
+                templateColumns='repeat(5, 1fr)'
+            >
+                <GridItem>
+                    <Grid
+                        templateColumns='repeat(5, 1fr)'
+                    >
+                        <TabList>
+                            {doctors.map((doctor, idx) => {
+                                return (<Grid item><Tab onClick={() => grabAppointments(doctor.id)} style={{ marginRight: "10px" }} key={idx}>{doctor.firstName} {doctor.lastName}</Tab></Grid>)
+                            })}
+                        </TabList>
+                    </Grid>
+                </GridItem>
+                <GridItem>
+                    <TabPanels>
+                        {doctors.map((doctor, idx) => {
+                            return (
+                                <div>
+                                <TabPanel style={{ marginTop: "10px" }} key={idx}>
+                                    <h2>{doctor.email}</h2>
+                                    <TableContainer>
+                                        <Table variant='simple'>
+                                            <Thead>
+                                                <Tr>
+                                                    <Th>Name</Th>
+                                                    <Th>Time</Th>
+                                                    <Th>Kind</Th>
+                                                </Tr>
+                                            </Thead>
+                                            {appointments.map(appointment => {
+                                                return (
+                                                    <Tbody>
+                                                        <Tr>
+                                                            <Td>{appointment.name}</Td>
+                                                            <Td>{appointment.time}</Td>
+                                                            <Td>{appointment.kind}</Td>
+                                                        </Tr>
+                                                    </Tbody>
+                                                )
+                                            })}
+                                        </Table>
+                                    </TableContainer>
+                                </TabPanel>
+                                </div>
+                            )
+                        })}
+                    </TabPanels>
+                </GridItem>
+            </Grid>
         </Tabs>
     )
 }
